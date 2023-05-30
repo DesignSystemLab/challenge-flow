537bf8dc5431302d7ed4e1df0efeda01dcd030a4
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-05-30 20:12:48 +0900

    ✨ feat: delegated error responsibility using ErrorBoundary component and added common error message function. #9

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,39 @@
-import { useContext } from 'react';
+import { Button } from '@jdesignlab/react';
+import { dehydrate, QueryClient } from 'react-query';
+import { ErrorModal } from '@shared/components/ErrorModal';
 import { SignupModal } from '@auth/components/SignupModal';
 import { SigninModal } from '@auth/components/SigninModal';
-import { Button } from '@jdesignlab/react';
-import { AuthContext } from '@shared/contexts/AuthContext';
+import { ErrorBoundary } from 'react-error-boundary';
+import { useUserAuth } from '@auth/hooks/useUserAuth';
+import { ImageUpload } from '@shared/components/ImageUpload';
 import { AuthMachineContext } from '@shared/contexts/AuthMachineContext';
-import { useActor } from '@xstate/react';
-import { fetchSignout } from '@auth/remotes/fetchSignout';
 
 const index = () => {
-  // const authContext = useContext(AuthContext);
-  // const [state] = useActor(authContext.authService);
-  const [state, send] = AuthMachineContext.useActor();
-  console.log(state.context.userInfo);
+  // const [state, send] = AuthMachineContext.useActor();
+  const { user } = useUserAuth();
 
   return (
     <div>
-      {state.value === 'loggedIn' && (
-        <>
-          <h2>{state.context.userInfo && `${state.context.userInfo.displayName}님 환영합니다.`}</h2>
-          <Button
-            onClick={() => {
-              fetchSignout(send);
-            }}
-          >
-            로그아웃
-          </Button>
-        </>
-      )}
-      <SigninModal />
-      <SignupModal />
+      {/* <ImageUpload /> */}
+      <ErrorBoundary FallbackComponent={ErrorModal}>
+        {user ? (
+          <>
+            {/* <h2>{state.context.userInfo && `${state.context.userInfo.displayName}님 환영합니다.`}</h2> */}
+            <Button
+            // onClick={() => {
+            //   fetchSignout(send);
+            // }}
+            >
+              로그아웃
+            </Button>
+          </>
+        ) : (
+          <>
+            <SigninModal />
+            <SignupModal />
+          </>
+        )}
+      </ErrorBoundary>
     </div>
   );
 };
