c659b1f17d991dcd18ddc3c435401edf96f56ba0
jiyeon <jy7828@gmail.com>
2023-05-30 17:24:44 +0900

    ✨ feat: add error boundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,30 @@ import type { AppProps } from 'next/app';
 import { Header } from '@layout/components/Header';
 import { Footer } from '@layout/components/Footer';
 import { mainWrapper } from '@layout/styles/main-style';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { GlobalErrorBoundary } from '@shared/components/GlobalErrorBoundary';
+import { Suspense } from 'react';
 
 const ChallengeFlow = ({ Component, pageProps }: AppProps) => {
   const queryClient = new QueryClient();
+  queryClient.setDefaultOptions({
+    queries: {
+      suspense: true
+    }
+  });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Header />
-      <main css={mainWrapper}>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </QueryClientProvider>
+    <GlobalErrorBoundary fallback={<div>error!</div>}>
+      <QueryClientProvider client={queryClient}>
+        {/* <Suspense fallback={<div>loading...</div>}> */}
+        <Header />
+        <main css={mainWrapper}>
+          <Component {...pageProps} />
+        </main>
+        {/* </Suspense> */}
+        <Footer />
+      </QueryClientProvider>
+    </GlobalErrorBoundary>
   );
 };
 
