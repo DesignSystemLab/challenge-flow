622e9d559146a490e368b5a467b6154a0f53ce19
jiyeon <jy7828@gmail.com>
2023-05-30 17:25:50 +0900

    ➖ remove: unnecessary code

--- src/pages/challenge/new.tsx
+++ src/pages/challenge/new.tsx
@@ -1,13 +1,9 @@
 import { CreateChallengeForm } from '@challenge/components/CreateChallengeForm';
-import { useRouter } from 'next/router';
 
 const newChallengePost = () => {
-  const router = useRouter();
-  const query = router.query;
-  const id = query.id as string;
   return (
     <>
-      <CreateChallengeForm id={id} />
+      <CreateChallengeForm />
     </>
   );
 };
