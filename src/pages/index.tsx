78cf1b5595d705197d8ac3576ad46a7104a3d59d
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-06-06 03:03:25 +0900

    ➖ remove: remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,12 @@
 import { ErrorModal } from '@shared/components/ErrorModal';
-import { SignupModal } from '@auth/components/SignupModal';
-import { SigninModal } from '@auth/components/SigninModal';
-import { useUserAuth } from '@auth/hooks/useUserAuth';
+import { Loading } from '@shared/components/Icons';
 import { ErrorBoundary } from 'react-error-boundary';
-import { Button } from '@jdesignlab/react';
 
-const index = () => {
-  // const [state, send] = AuthMachineContext.useActor();
-  const { data: user } = useUserAuth();
-
-  return (
-    <div>
-      {/* <ImageUpload /> */}
-      <ErrorBoundary FallbackComponent={ErrorModal}>
-        {user ? (
-          <>
-            {/* <h2>{state.context.userInfo && `${state.context.userInfo.displayName}님 환영합니다.`}</h2> */}
-            <Button
-            // onClick={() => {
-            //   fetchSignout(send);
-            // }}
-            >
-              로그아웃
-            </Button>
-          </>
-        ) : (
-          <>
-            <SigninModal />
-            <SignupModal />
-          </>
-        )}
-      </ErrorBoundary>
-    </div>
-  );
-};
+const index = () => (
+  <div>
+    <Loading />
+    <ErrorBoundary FallbackComponent={ErrorModal} />
+  </div>
+);
 
 export default index;
