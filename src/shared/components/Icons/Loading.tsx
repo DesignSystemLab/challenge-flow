5ef01eed366935d0902c1f3b94e69c7f77217b49
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-06-06 00:50:41 +0900

    💄 ui: change loading component

--- src/shared/components/Icons/Loading.tsx
+++ src/shared/components/Icons/Loading.tsx
@@ -1,9 +1,21 @@
 export const Loading = () => (
-  <svg width="24px" height="24px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    style={{
+      margin: 'auto',
+      backgroundColor: 'transparent',
+      display: 'block',
+      shapeRendering: 'auto'
+    }}
+    width="24px"
+    height="24px"
+    viewBox="0 0 100 100"
+    preserveAspectRatio="xMidYMid"
+  >
     <g>
-      <path d="M50 15A35 35 0 1 0 74.74873734152916 25.251262658470843" fill="none" stroke="#e3e3e3" strokeWidth="7" />
-      <path d="M49 3L49 27L61 15L49 3" fill="#e3e3e3" />
-      <g
+      <path d="M50 15A35 35 0 1 0 74.74873734152916 25.251262658470843" fill="none" stroke="#dfe4ea" strokeWidth="12" />
+      <path d="M49 3L49 27L61 15L49 3" fill="#dfe4ea" />
+      <animateTransform
         attributeName="transform"
         type="rotate"
         repeatCount="indefinite"
