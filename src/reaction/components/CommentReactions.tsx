cf59f16d074a611a0ec763638c90cb764fcbb28d
jiyeon <jy7828@gmail.com>
2023-06-15 00:10:34 +0900

    🐛 fix: change useCommonQuery to useQuery

--- src/reaction/components/CommentReactions.tsx
+++ src/reaction/components/CommentReactions.tsx
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
+import { useQuery } from 'react-query';
 import { EachComment } from './EachComment';
 import { CommentTextarea } from './CommentTextarea';
-import { useCommonQuery } from '../hooks/useCommentApi';
 import fetchReadListComment from '../remotes/fetchReadCommentList';
 import { useUpdateComment } from '../hooks/useUpdateComment';
 import { CommentFields } from '../types';
 
 export const CommentReactions = ({ originId }: { originId: string }) => {
   const [editMode, setEditMode] = useState<string | null>(null);
-  const { data } = useCommonQuery(`comment-${originId}`, () => fetchReadListComment({ originId }));
+  const { data } = useQuery(`comment-${originId}`, () => fetchReadListComment({ originId }));
 
   const { onUpdateComment } = useUpdateComment(originId, setEditMode);
 
   return (
     <>
       <CommentTextarea originId={originId} />
-      {data.map((comment: CommentFields) => (
-        <div key={comment.id}>
-          <EachComment data={comment} onUpdateComment={onUpdateComment} editMode={editMode} setEditMode={setEditMode} />
-        </div>
-      ))}
+      {data &&
+        data.length > 0 &&
+        data.map((comment: CommentFields) => (
+          <div key={comment.id}>
+            <EachComment
+              data={comment}
+              onUpdateComment={onUpdateComment}
+              editMode={editMode}
+              setEditMode={setEditMode}
+            />
+          </div>
+        ))}
     </>
   );
 };
