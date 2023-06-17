fed512017e2b645f1c26c51c38faeb7e6ba3bb8a
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-06-18 01:07:25 +0900

    🏗️ structure: change directory structure of the boundary folder

--- src/pages/workspace/index.tsx
+++ src/pages/workspace/index.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react';
 import { sidebarStyle } from '@workspace/styles/sidebarStyle';
 import { Sidebar } from '@workspace/components/Sidebar';
 import { TurmRangeSelector } from '@workspace/components/TurmRangeSelector';
@@ -6,20 +5,25 @@ import { GroupStatistics } from '@workspace/components/GroupStatistics';
 import { Posts } from '@workspace/components/Posts';
 import { workspaceLayout } from '@workspace/styles/layout';
 import { workspaceMainWrapper } from '@workspace/styles/workspaceStyle';
-import DynamicWrapper from '@shared/components/DynamicWrapper';
 import { ProfileSkeleton } from '@shared/components/suspense/ProfileSkeleton';
+import { CompositionBoundaryReactQuery } from '@shared/boundaries/';
+import { CommonErrorFallback } from '@shared/components/CommonErrorFallback';
 
 const WorkspacePage = () => (
   <main css={workspaceLayout}>
     <aside css={sidebarStyle}>
-      <DynamicWrapper>
-        <Suspense fallback={<ProfileSkeleton count={4} />}>
-          <Sidebar />
-        </Suspense>
-        <Suspense fallback={<h2>loading.3.</h2>}>
-          <TurmRangeSelector />
-        </Suspense>
-      </DynamicWrapper>
+      <CompositionBoundaryReactQuery
+        suspense={<ProfileSkeleton count={4} />}
+        error={(prop) => <CommonErrorFallback {...prop} />}
+      >
+        <Sidebar />
+      </CompositionBoundaryReactQuery>
+      <CompositionBoundaryReactQuery
+        suspense={<ProfileSkeleton count={4} />}
+        error={(prop) => <CommonErrorFallback {...prop} />}
+      >
+        <TurmRangeSelector />
+      </CompositionBoundaryReactQuery>
     </aside>
     <section css={workspaceMainWrapper}>
       <GroupStatistics />
