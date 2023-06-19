e83aeac4c2d55346c458932d02af8bee6217fbf4
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-06-20 04:54:49 +0900

    💄 ui: add ProfileSkeleton padding

--- src/shared/components/suspense/ProfileSkeleton.tsx
+++ src/shared/components/suspense/ProfileSkeleton.tsx
@@ -10,7 +10,7 @@ export const ProfileSkeleton = (props: Props) => {
     .map((_, index) => ({ id: index + 1 }));
 
   return (
-    <ul css={{ padding: 0, margin: 0 }}>
+    <ul css={{ padding: '8px 0', margin: 0, backgroundColor: 'white' }}>
       {skleletons.map((item) => (
         <li key={item.id} css={{ padding: '4px', display: 'flex', width: '256px', minHeight: '36px', gap: '8px' }}>
           <div css={{ flexGrow: 2, maxWidth: '24px', minWidth: '64px' }}>
