0ff1d9adbc0a5c2666dd320ac90d2ddc98e44806
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-02 23:55:28 +0900

    ✨ feat: set firebase-admin configure #33

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -1,7 +1,15 @@
-import { initializeApp } from 'firebase/app';
+import { getApps, getApp, initializeApp } from 'firebase/app';
+import * as admin from 'firebase-admin';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import type { ServiceAccount } from 'firebase-admin';
+
+const cert: ServiceAccount = {
+  clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+  privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID
+};
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,10 +20,10 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
-
-const app = initializeApp(firebaseConfig);
+const friebaseAdmin = admin.apps[0] ?? admin.initializeApp({ credential: admin.credential.cert(cert) });
+const app = getApps.length ? getApp() : initializeApp(firebaseConfig);
 const database = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export { app, database, auth, storage };
+export { app, database, auth, storage, friebaseAdmin };
