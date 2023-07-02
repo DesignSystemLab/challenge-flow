d4d179a81a13ee22d45f8cb6e5fdfdf75f7853d7
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-03 05:11:43 +0900

    ✨ feat: register session after social login in Provider #33

--- src/auth/hooks/useSigninWithProvider.ts
+++ src/auth/hooks/useSigninWithProvider.ts
@@ -1,5 +1,6 @@
 import { useErrorBoundary } from 'react-error-boundary';
 import { useMutation } from 'react-query';
+import { signIn } from 'next-auth/react';
 import { useSetUserAuthData } from './useSetUserAuthData';
 import { fetchAuthWithGoogle } from '../remotes/fetchAuthWithGoogle';
 import { fetchAuthWithGithub } from '../remotes/fetchAuthWithGithub';
@@ -10,9 +11,17 @@ export const useSigninWithProvider = (providerType: AuthProvider, send: (props: 
   const { showBoundary } = useErrorBoundary();
   const fetchAuthFuntion = providerType === 'GITHUB' ? fetchAuthWithGithub : fetchAuthWithGoogle;
   const { mutate, isLoading } = useMutation(fetchAuthFuntion, {
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
       if (data) {
+        const tokenId = await data.getIdToken(true);
         updateUserData(data);
+        signIn(
+          'credentials',
+          {
+            callbackUrl: '/'
+          },
+          { tokenId, providerType }
+        );
         send({ type: 'CLEAR' });
         return;
       }
