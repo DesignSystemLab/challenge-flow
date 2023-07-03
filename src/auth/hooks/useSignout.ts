5a7047cd1c9faa97b5345c9d61fa9359686b9538
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-04 01:55:57 +0900

    ✨ feat: clear session upon logout

--- src/auth/hooks/useSignout.ts
+++ src/auth/hooks/useSignout.ts
@@ -1,6 +1,7 @@
 import { clearLocalStorageItem } from '@shared/storage';
 import { STORAGE_KEYS, QUREY_KEYS } from '@shared/constants';
 import { useMutation, useQueryClient } from 'react-query';
+import { signOut } from 'next-auth/react';
 import { fetchSignout } from '../remotes/fetchSignout';
 
 export const useSignout = () => {
@@ -9,6 +10,7 @@ export const useSignout = () => {
   const { user } = QUREY_KEYS;
   const { mutate, isLoading } = useMutation(fetchSignout, {
     onSuccess: () => {
+      signOut();
       clearLocalStorageItem(userAuth);
       queryClient.setQueryData(user, null);
     }
