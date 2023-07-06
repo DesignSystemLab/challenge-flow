76d7574a8f3d44f0d8b62cd8e69ac367c487471f
jiyeon <jy7828@gmail.com>
2023-07-07 01:09:36 +0900

    🐛 fix: rollback wrong merged code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,16 @@ import { ChallengeListError } from '@challenge/components/Error';
 import { ChallengeFilter } from '@challenge/components/ChallengeFilter';
 import { CompositionBoundaryReactQuery } from '@shared/boundaries';
 import { Loading } from '@shared/components/Icons';
+import { UserSession } from '@challenge/types';
 import { useRouter } from 'next/router';
 import { Button } from '@jdesignlab/react';
 import { EllipsisHorizontal } from '@jdesignlab/react-icons';
+import { useSession } from 'next-auth/react';
 
 const MainPage = () => {
+  const { data } = useSession();
+  const userSession = data as unknown as UserSession;
+
   const router = useRouter();
 
   const moveToChallengeList = () => {
@@ -28,7 +33,7 @@ const MainPage = () => {
               더 보러 가기
             </Button>
           </div>
-          <ChallengeList showTotalCount={false} />
+          <ChallengeList currentUser={userSession?.user} showTotalCount />
         </div>
       </CompositionBoundaryReactQuery>
     </div>
