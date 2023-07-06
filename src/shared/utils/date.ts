f071e9c76c7a552dff47337361fdf533e5caad5a
jiyeon <jy7828@gmail.com>
2023-07-07 00:21:06 +0900

    ✨ feat: util date function

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -4,7 +4,11 @@ import {
   differenceInMonths,
   differenceInDays,
   differenceInHours,
-  differenceInMinutes
+  differenceInMinutes,
+  addDays,
+  addWeeks,
+  isBefore,
+  isAfter
 } from 'date-fns';
 
 export const getTimeDiff = (time: string) => {
@@ -45,4 +49,47 @@ export const calculateDayDiff = (start: string, end: string, type?: CalculateDat
   const dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
   return type === 'week' ? Math.floor(dayDiff / 7) : dayDiff;
+};
+
+type Datetype = 'day' | 'week';
+export const addDate = (fromDate: string | Date | number, amount: number, type?: Datetype) => {
+  if (type === 'week') {
+    return addWeeks(getDate(fromDate), amount);
+  }
+  return addDays(getDate(fromDate), amount);
+};
+
+/**
+ * @description 날짜 형식을 변경합니다.
+ * @param date Date
+ * @param dateType: - , / , ko
+ */
+type FormatType = '-' | '/' | 'ko';
+export const formatDate = (date: Date, type?: FormatType) => {
+  const convertDate = formatDateToYYYYMMDD(date);
+
+  const year = convertDate.substring(0, 4);
+  const month = convertDate.substring(4, 6);
+  const day = convertDate.substring(6, 8);
+
+  switch (type) {
+    case '-':
+      return `${year}-${month}-${day}`;
+    case '/':
+      return `${year}/${month}/${day}`;
+    default:
+      return `${year}년 ${month}월 ${day}일`;
+  }
+};
+
+export const isEarlierThanNow = (date: string) => {
+  const now = getDate();
+  const inputDate = getDate(date);
+  return isBefore(now, inputDate);
+};
+
+export const isAfterThanNow = (date: string) => {
+  const now = getDate();
+  const inputDate = getDate(date);
+  return isAfter(now, inputDate);
 };
