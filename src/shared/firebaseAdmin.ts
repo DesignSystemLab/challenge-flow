647b9aa018a520e0cb294ff7d627df33d391eb94
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-07 02:08:35 +0900

    🐛 fix: replace breakline from privatekey

--- src/shared/firebaseAdmin.ts
+++ src/shared/firebaseAdmin.ts
@@ -1,21 +1,10 @@
 import * as admin from 'firebase-admin';
 import type { ServiceAccount } from 'firebase-admin';
 
-interface PrivateKeyType {
-  privateKey: string;
-}
-
-let parseKey = '';
-const privateEnv = process.env.NEXT_PUBLIC_PRIVATE_KEY;
-if (privateEnv) {
-  const { privateKey } = JSON.parse(privateEnv) as PrivateKeyType;
-  parseKey = privateKey;
-}
-
 const cert: ServiceAccount = {
   clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  privateKey: parseKey
+  privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY ? process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/gm, '\n') : ''
 };
 
 const friebaseAdmin = admin.apps[0] ?? admin.initializeApp({ credential: admin.credential.cert(cert) });
