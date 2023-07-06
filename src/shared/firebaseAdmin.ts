f0bfe731361b0e253d220361cecd92910badbaef
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-07 01:54:25 +0900

    🐛 fix: parse firebase Private key

--- src/shared/firebaseAdmin.ts
+++ src/shared/firebaseAdmin.ts
@@ -1,10 +1,21 @@
 import * as admin from 'firebase-admin';
 import type { ServiceAccount } from 'firebase-admin';
 
+interface PrivateKeyType {
+  privateKey: string;
+}
+
+let parseKey = '';
+const privateEnv = process.env.NEXT_PUBLIC_PRIVATE_KEY;
+if (privateEnv) {
+  const { privateKey } = JSON.parse(privateEnv) as PrivateKeyType;
+  parseKey = privateKey;
+}
+
 const cert: ServiceAccount = {
   clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-  privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  privateKey: parseKey
 };
 
 const friebaseAdmin = admin.apps[0] ?? admin.initializeApp({ credential: admin.credential.cert(cert) });
