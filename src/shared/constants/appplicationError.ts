2b7f9f13a5cc21419d6a93443e345d003d9cc351
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-09 17:50:50 +0900

    🔧 modify: add error message for 401 HTTP status

--- src/shared/constants/appplicationError.ts
+++ src/shared/constants/appplicationError.ts
@@ -1,4 +1,13 @@
-type AppErrorCode = 'SERVER' | 'UNKNOWN' | 'FIREBASE' | 'FROBIDDEN' | 'NOTFOUND' | 'UNAUTHORIZED' | 'BADREQUEST';
+type AppErrorCode =
+  | 'SERVER'
+  | 'UNKNOWN'
+  | 'FIREBASE'
+  | 'FROBIDDEN'
+  | 'NOTFOUND'
+  | 'UNAUTHORIZED'
+  | 'BADREQUEST'
+  | 'NOTALLOWED';
+
 export const ApplicationError: Record<AppErrorCode, string> = {
   FIREBASE: '파이어베이스 통신 중 오류가 발생하였습니다.', // 503
   SERVER: '내부 서버에서 오류가 발생하였습니다.', // 500
@@ -6,5 +15,6 @@ export const ApplicationError: Record<AppErrorCode, string> = {
   BADREQUEST: '잘못된 요청입니다.', // 400
   UNAUTHORIZED: '인증되지 않은 사용자입니다.', // 401
   FROBIDDEN: '요청이 거부되었습니다.', // 403
-  NOTFOUND: '요청 주소가 존재하지 않습니다.' // 404
+  NOTFOUND: '요청 주소가 존재하지 않습니다.', // 404
+  NOTALLOWED: '요청 메서드가 잘못되었습니다.' // 405
 };
