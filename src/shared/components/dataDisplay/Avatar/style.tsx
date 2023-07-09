14a844648fe2776c64c764314c86dc96b2eec128
jiyeon <jy7828@gmail.com>
2023-07-10 06:37:17 +0900

    ✨ feat: update Avatar to get image #40

--- src/shared/components/dataDisplay/Avatar/style.tsx
+++ src/shared/components/dataDisplay/Avatar/style.tsx
@@ -1,13 +1,9 @@
-type AvatarSize = 'sm' | 'md' | 'lg';
-export const commentProfileAvatar = (size: AvatarSize) => {
-  let avatarSize = '40px';
-  if (size === 'sm') avatarSize = '30px';
-  if (size === 'lg') avatarSize = '50px';
+import { parseSize } from './parseSize';
+import { AvatarSize } from './type';
 
-  return {
-    borderRadius: '50%',
-    border: 'solid #e0e0e0 1px',
-    width: avatarSize,
-    height: avatarSize
-  };
-};
+export const commentProfileAvatar = (size: AvatarSize) => ({
+  borderRadius: '50%',
+  border: 'solid #e0e0e0 1px',
+  width: parseSize(size),
+  height: parseSize(size)
+});
---
+++ src/shared/components/dataDisplay/Avatar/type.ts
@@ -0,0 +1,1 @@
+export type AvatarSize = 'sm' | 'md' | 'lg';
