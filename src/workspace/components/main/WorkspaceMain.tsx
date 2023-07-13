a40272028bf7adb1e01a20c081657c4af372f4d6
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-14 02:39:22 +0900

    ✨ feat:implemented the post statistics component (BarChart) #46

--- src/workspace/components/main/WorkspaceMain.tsx
+++ src/workspace/components/main/WorkspaceMain.tsx
@@ -9,9 +9,11 @@ import { TabList } from './TabList';
 export const WorkspaceMain = () => (
   <div css={workspaceMainWrapper}>
     <Flex direction="column" style={{ border: '1px solid red', height: '100%' }}>
-      <Flex.Item flex={1} style={{ height: '50%' }}>
-        <GroupStatistics />
-      </Flex.Item>
+      <CompositionBoundaryReactQuery suspense={<Loader />} error={(prop) => <CommonErrorFallback {...prop} />}>
+        <Flex.Item flex={1} style={{ height: '50%' }}>
+          <GroupStatistics />
+        </Flex.Item>
+      </CompositionBoundaryReactQuery>
       <CompositionBoundaryReactQuery suspense={<Loader />} error={(prop) => <CommonErrorFallback {...prop} />}>
         <Flex.Item flex={1} style={{ height: '50%' }}>
           <TabList />
