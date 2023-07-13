48a8fcf39f6a58dcbdd551797dc30d4b4c691b05
jiyeon <jy7828@gmail.com>
2023-07-14 07:22:06 +0900

    🐛 fix: change the way to get data in serversideprops

--- src/pages/challenge/[id]/modify.tsx
+++ src/pages/challenge/[id]/modify.tsx
@@ -1,10 +1,11 @@
 import { ParsedUrlQuery } from 'querystring';
 import { CreateChallengeForm } from '@challenge/components/CreateChallengeForm';
+import { database } from '@shared/firebase';
 import { HasValidSession } from '@shared/components/HasValidSession';
-import { ChallengeAPI } from '@challenge/remotes';
-import { ChallengeModifyFetchProps } from '@challenge/types';
+import { ChallengeModifyFetchProps, ChallengePostFields } from '@challenge/types';
 import { GetServerSideProps } from 'next';
 import { useSession } from 'next-auth/react';
+import { doc, getDoc } from 'firebase/firestore';
 
 interface QueryInterface extends ParsedUrlQuery {
   id?: string;
@@ -23,8 +24,20 @@ interface UserSession {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.params as QueryInterface;
   if (id) {
-    const postInfo = await ChallengeAPI.getPostDetail(id);
-    return { props: { postInfo } };
+    const REF_NAME = 'challenge';
+    const docRef = doc(database, REF_NAME, id);
+    const docSnapshot = await getDoc(docRef);
+
+    if (docSnapshot.exists()) {
+      const postInfo = {
+        ...docSnapshot.data(),
+        userId: docSnapshot.data().userId.id,
+        members: docSnapshot.data().members.map((d: ChallengePostFields) => d.id),
+        likes: docSnapshot.data().likes.map((d: ChallengePostFields) => d.id),
+        id: docSnapshot.id
+      };
+      return { props: { postInfo } };
+    }
   }
   return { props: {} };
 };
