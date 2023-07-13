02457e94c7f3e3bbc2de7dd3278ce100120814b2
jiyeon <jy7828@gmail.com>
2023-07-13 12:32:12 +0900

    ✨ feat: make workspace feature #7

--- src/challenge/components/StartButton.tsx
+++ src/challenge/components/StartButton.tsx
@@ -1,18 +1,21 @@
-import { isEarlierThanNow } from '@shared/utils/date';
-import { ChallengeModifyFetchProps } from '@challenge/types';
+import { useContext } from 'react';
+import { ChallengeContext } from '@challenge/context';
+import { useStartMutation } from '@challenge/hooks/useStartMutation';
 import { Button } from '@jdesignlab/react';
 import { Plus } from '@jdesignlab/react-icons';
 
-interface Props {
-  postInfo: ChallengeModifyFetchProps;
-}
+export const StartButton = () => {
+  const { postInfo } = useContext(ChallengeContext);
 
-export const StartButton = ({ postInfo }: Props) => (
-  <Button
-    disabled={isEarlierThanNow(postInfo.dueAt)}
-    size="lg"
-    icon={<Plus width={16} height={16} style={{ marginBottom: '2px' }} />}
-  >
-    워크스페이스 생성
-  </Button>
-);
+  const startSuccessAction = async () => {};
+  const { startAction } = useStartMutation(startSuccessAction);
+  const onClickStart = () => {
+    startAction(postInfo.id);
+  };
+
+  return (
+    <Button onClick={onClickStart} size="lg" icon={<Plus width={16} height={16} style={{ marginBottom: '2px' }} />}>
+      워크스페이스 생성
+    </Button>
+  );
+};
