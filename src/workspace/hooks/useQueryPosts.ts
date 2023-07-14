ec7d70259542e46b26711f1b9d21c27ba70d38f2
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-14 09:18:47 +0900

    🔧 modify:  add dependency on workspaceId to the workspacePostsByPeriod query.

--- src/workspace/hooks/useQueryPosts.ts
+++ src/workspace/hooks/useQueryPosts.ts
@@ -4,9 +4,13 @@ import { fetchPosts } from '../remotes/fetchPosts';
 
 export const useQueryPosts = (period: string, workspaceId: string) => {
   const { workspacePostsByPeriod } = QUERY_KEYS;
-  const { data, refetch } = useQuery([workspacePostsByPeriod, period], () => fetchPosts(period, workspaceId), {
-    staleTime: 0,
-    cacheTime: 0
-  });
+  const { data, refetch } = useQuery(
+    [workspacePostsByPeriod, period, workspaceId],
+    () => fetchPosts(period, workspaceId),
+    {
+      staleTime: 0,
+      cacheTime: 0
+    }
+  );
   return { data, refetch };
 };
