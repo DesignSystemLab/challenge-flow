a6c3565607ee58127d436e62eb452d9e712c3712
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-24 02:40:23 +0900

    ➖ remove: removed comments

--- src/shared/queryClient.ts
+++ src/shared/queryClient.ts
@@ -1,20 +1,5 @@
 import { QueryClient } from 'react-query';
-// import { FirebaseError } from 'firebase/app';
 
-// const errorHandler = (error: unknown) => {
-//   if (error instanceof FirebaseError) {
-//     const firebaseError = error as FirebaseError;
-//     const { code = 'NotFound', message = '알 수 없는 오류입니다.' } = firebaseError;
-//   }
-//   if (error instanceof Error) {
-//   }
-// };
-
-// const localStoragePersistor = createWebStoragePersistor({
-//   storage: window.localStorage
-// });
-
-// to satisfy typescript until this file has uncommented contents
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -23,9 +8,3 @@ export const queryClient = new QueryClient({
     mutations: {}
   }
 });
-
-// persistQueryClient({
-//   queryClient,
-//   persistor: localStoragePersistor,
-//   maxAge: cacheTime
-// });
