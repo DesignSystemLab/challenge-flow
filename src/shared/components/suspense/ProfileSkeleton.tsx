777d652c6f857134c222699afbd471c3a0aa304f
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-05 03:25:34 +0900

    🎨 style: fix typo

--- src/shared/components/suspense/ProfileSkeleton.tsx
+++ src/shared/components/suspense/ProfileSkeleton.tsx
@@ -5,13 +5,13 @@ interface Props {
 }
 export const ProfileSkeleton = (props: Props) => {
   const { count = 1 } = props;
-  const skleletons = Array(count)
+  const skeletons = Array(count)
     .fill(null)
     .map((_, index) => ({ id: index + 1 }));
 
   return (
     <ul css={{ padding: '8px 0', margin: 0, backgroundColor: 'white' }}>
-      {skleletons.map((item) => (
+      {skeletons.map((item) => (
         <li key={item.id} css={{ padding: '4px', display: 'flex', width: '256px', minHeight: '36px', gap: '8px' }}>
           <div css={{ flexGrow: 2, maxWidth: '24px', minWidth: '64px' }}>
             <Skeleton circle width="64px" height="64px" animation />
