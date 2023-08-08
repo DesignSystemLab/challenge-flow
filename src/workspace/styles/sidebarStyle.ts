29ef0adafce3d180ca847c84501f958e2a69a9d1
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-09 02:26:20 +0900

    💄 ui: enhanced the mobile UI for the workspace group page #53

--- src/workspace/styles/sidebarStyle.ts
+++ src/workspace/styles/sidebarStyle.ts
@@ -3,62 +3,66 @@ import { SCROLLBAR_COLOR } from '@layout/constant';
 import { css } from '@emotion/react';
 
 export const sidebarStyle = css({
-  [mq.md]: {},
+  flexGrow: 0,
+  margin: 0,
+  backgroundColor: '#FAF8FF',
+  marginBottom: '36px',
+  marginTop: '16px',
   [mq.lg]: {
-    boxShadow: 'rgba(9, 30, 66, 0.25) 0px 0px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 0px',
-    backgroundColor: '#FAF8FF',
+    marginBottom: 0,
     maxWidth: '256px',
     height: '100%',
     flexGrow: 1
-  },
-  [mq.xl]: {}
+  }
 });
 
 export const sidebarSectionStyle = css({
   position: 'static',
-  [mq.md]: {},
+  '&::-webkit-scrollbar': {
+    width: '8px',
+    background: '#ffffff'
+  },
+  '&::-webkit-scrollbar-thumb': {
+    borderRadius: '4px',
+    background: `${SCROLLBAR_COLOR}`
+  },
   [mq.lg]: {
     position: 'relative',
     height: 'calc((100% - 72px) / 2)',
     overflowY: 'auto',
-    margin: 0,
-    '&::-webkit-scrollbar': {
-      width: '8px',
-      background: '#ffffff'
-    },
-    '&::-webkit-scrollbar-thumb': {
-      borderRadius: '5px',
-      background: `${SCROLLBAR_COLOR}`
-    }
+    margin: 0
   }
 });
 
 export const memberListStyle = css({
-  height: '100%',
-  padding: '0',
   margin: 0,
-  overflowY: 'auto'
+  padding: 0,
+  [mq.md]: {
+    height: '100%',
+    padding: '0',
+    margin: 0,
+    overflowY: 'auto'
+  }
 });
 
 export const memberCardStyle = css({
-  backgroundColor: '#fff',
-  boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+  backgroundColor: '#FAF8FF',
   display: 'flex',
   padding: '4px 8px',
-  [mq.md]: {},
-  [mq.lg]: {},
-  [mq.xl]: {}
+  [mq.lg]: {
+    backgroundColor: '#fff',
+    boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px'
+  }
 });
 
 export const profileStyle = css({
-  [mq.md]: {},
+  marginLeft: '8px',
   [mq.lg]: {
     width: '160px',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'start',
-    marginLeft: '8px',
     '& p': {
       width: '100%',
       whiteSpace: 'nowrap',
@@ -67,12 +71,10 @@ export const profileStyle = css({
       wordBreak: 'break-all',
       WebkitLineClamp: 2
     }
-  },
-  [mq.xl]: {}
+  }
 });
 
 export const scheduleListStyle = css({
-  backgroundColor: 'red',
   padding: '0',
   margin: '0'
 });
@@ -83,12 +85,10 @@ export const selectorStyle = css({
     width: '100%',
     height: 'auto'
   },
-  [mq.md]: {},
   [mq.lg]: {
     padding: '4px',
     cursor: 'pointer',
     backgroundColor: '#fff',
     boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px'
-  },
-  [mq.xl]: {}
+  }
 });
--- src/workspace/styles/workspaceStyle.ts
+++ src/workspace/styles/workspaceStyle.ts
@@ -1,10 +1,22 @@
 import { mq } from '@shared/styles/mixins/responsive';
+import { BORDER_COLOR } from '@layout/constant';
 import { css } from '@emotion/react';
 
 export const workspaceMainWrapper = css({
-  [mq.md]: {},
+  border: `1px solid ${BORDER_COLOR}`,
+  borderRadius: '4px',
+  padding: '16px 0',
+  margin: 0,
   [mq.lg]: {
+    margin: '16px 0 0 16px',
     flexGrow: 8
-  },
-  [mq.xl]: {}
+  }
 });
+
+export const workspaceFlexItemStyle = (height: string) =>
+  css({
+    height: 'auto',
+    [mq.lg]: {
+      height
+    }
+  });
